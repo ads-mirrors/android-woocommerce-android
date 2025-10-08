30e00f91747842eeecb07d1b5394875201fa0824
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-10-08 12:33:53 +0200

    Handle one pane to two pane transition

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/bookings/details/BookingDetailsFragment.kt
@@ -5,9 +5,14 @@ import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.woocommerce.android.R
+import com.woocommerce.android.extensions.isTwoPanesShouldBeUsed
 import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.bookings.BookingsCommunicationViewModel
 import com.woocommerce.android.ui.compose.composeView
 import com.woocommerce.android.ui.main.AppBarStatus
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +22,8 @@ import kotlinx.parcelize.Parcelize
 class BookingDetailsFragment : BaseFragment() {
 
     private val viewModel: BookingDetailsViewModel by viewModels()
+    private val navArgs: BookingDetailsFragmentArgs by navArgs()
+    private val bookingsCommunicationViewModel: BookingsCommunicationViewModel by activityViewModels()
 
     override val activityAppBarStatus: AppBarStatus
         get() = AppBarStatus.Hidden
@@ -33,6 +40,31 @@ class BookingDetailsFragment : BaseFragment() {
                     )
                 }
             )
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleOnePaneToTwoPaneConversion()
+    }
+
+    private fun handleOnePaneToTwoPaneConversion() {
+        val isScreenLargerThanCompact = requireContext().isTwoPanesShouldBeUsed
+        val isBookingListFragmentUpInBackStack =
+            findNavController().previousBackStackEntry?.destination?.id == R.id.bookingListFragment
+        if (isScreenLargerThanCompact && isBookingListFragmentUpInBackStack) {
+            when (val mode = navArgs.mode) {
+                is Mode.ShowBooking -> {
+                    findNavController().popBackStack()
+                    bookingsCommunicationViewModel.pushEvent(
+                        BookingsCommunicationViewModel.CommunicationEvent.BookingSelected(mode.bookingId)
+                    )
+                }
+
+                is Mode.Empty -> {
+                    findNavController().popBackStack()
+                }
+            }
         }
     }
 
